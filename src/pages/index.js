26740358b7add2298f29c0db1266af5f5708c7b7
Shawn <60294318+Shawn-sudo@users.noreply.github.com>
2022-04-21 11:55:37 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import Layout from "../components/Layout";
 import ChemOfLead from "../components/pages/home_page/chem_of_lead";
 import FlintWaterCrisis from "../components/pages/home_page/flint_water_crisis";
@@ -12,71 +12,71 @@ const pageContentLayoutStyles = {
   paddingTop: 96,
 };
 
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-};
-const headingAccentStyles = {
-  color: "#663399",
-};
-const paragraphStyles = {
-  marginBottom: 48,
-};
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-};
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-};
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-};
+// const headingStyles = {
+//   marginTop: 0,
+//   marginBottom: 64,
+//   maxWidth: 320,
+// };
+// const headingAccentStyles = {
+//   color: "#663399",
+// };
+// const paragraphStyles = {
+//   marginBottom: 48,
+// };
+// const codeStyles = {
+//   color: "#8A6534",
+//   padding: 4,
+//   backgroundColor: "#FFF4DB",
+//   fontSize: "1.25rem",
+//   borderRadius: 4,
+// };
+// const listStyles = {
+//   marginBottom: 96,
+//   paddingLeft: 0,
+// };
+// const listItemStyles = {
+//   fontWeight: 300,
+//   fontSize: 24,
+//   maxWidth: 560,
+//   marginBottom: 30,
+// };
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-};
+// const linkStyle = {
+//   color: "#8954A8",
+//   fontWeight: "bold",
+//   fontSize: 16,
+//   verticalAlign: "5%",
+// };
 
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-};
+// const docLinkStyle = {
+//   ...linkStyle,
+//   listStyleType: "none",
+//   marginBottom: 24,
+// };
 
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-};
+// const descriptionStyle = {
+//   color: "#232129",
+//   fontSize: 14,
+//   marginTop: 10,
+//   marginBottom: 0,
+//   lineHeight: 1.25,
+// };
 
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-};
+// const badgeStyle = {
+//   color: "#fff",
+//   backgroundColor: "#088413",
+//   border: "1px solid #088413",
+//   fontSize: 11,
+//   fontWeight: "bold",
+//   letterSpacing: 1,
+//   borderRadius: 4,
+//   padding: "4px 6px",
+//   display: "inline-block",
+//   position: "relative",
+//   top: -2,
+//   marginLeft: 10,
+//   lineHeight: 1,
+// };
 
 // // data
 // const links = [
